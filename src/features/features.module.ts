9eda085b9adaa5699cb5b7a84cfd323bbfb3b14f
Ganda1f <danylo.averbakh@lazy-ants.net>
2020-10-25 01:20:34 +0300

    init base struct

--- src/features/features.module.ts
+++ src/features/features.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '@app/features/auth/auth.module';
 import { UserModule } from './user/user.module';
+import { CultureModule } from './culture/culture.module';
+import { FieldModule } from './field/field.module';
+import { NoteModule } from './note/note.module';
+import { WorkspaceModule } from './workspace/workspace.module';
+import { MeasurementModule } from './measurement/measurement.module';
 
 const FEATURES = [
   AuthModule,
@@ -10,6 +15,11 @@ const FEATURES = [
 @Module({
   imports: [
     ...FEATURES,
+    CultureModule,
+    FieldModule,
+    NoteModule,
+    WorkspaceModule,
+    MeasurementModule,
   ],
   exports: [
     ...FEATURES
